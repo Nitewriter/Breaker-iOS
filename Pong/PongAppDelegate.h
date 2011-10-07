010b75aa888b333642636bb845136e09202a0e22
Joel Garrett <lythium.ion@gmail.com>
2011-10-06 20:59:30 -0400

    Initial Commit

---
+++ Pong/PongAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PongAppDelegate.h
+//  Pong
+//
+//  Created by Joel Garrett on 10/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PongAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
